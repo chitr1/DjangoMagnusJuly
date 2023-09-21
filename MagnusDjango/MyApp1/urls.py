deca1061411b11cf978dd4dee0c5e46e343d0a61
chit1 <citrabal@gmail.com>
2023-09-21 14:03:27 -0500

    Django Day 5

--- MagnusDjango/MyApp1/urls.py
+++ MagnusDjango/MyApp1/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('help/',views.help,name='help'),
     path('contact/',views.contact,name='contact'),
     path('emp/',views.emp,name="emp"),
-    path('dept/',views.dept,name="dept")
+    path('dept/',views.dept,name="dept"),
+    path('user/',views.form_page,name='UserForm'),
+    path('empform/',views.emp_formpage,name="empform"),
+
+
 
 ]
